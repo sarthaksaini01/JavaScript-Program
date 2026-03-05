3e374af3ef4ee1d58ed285b112085206b7bd969b
SARTHAK SAINI <164344547+sarthaksaini01@users.noreply.github.com>
2026-03-05 22:22:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'POST',
-  body: JSON.stringify({
-    title: 'foo',
-    body: 'bar',
-    userId: 1,
-  }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+const typingtext = document.querySelector('typing text p');
+const input = document.querySelector('.wrapper .inputfield');
+const time = document.querySelector('.time span b');
+const mistake  = document.querySelector('.mistake span');
+const wpm = document.querySelector('.wpm span');
+const cpm = document.querySelector('.cpm span');
+const Btn = document.querySelector('button');
+
+function loadparagraph(){
+    const paragraph=["The sun dipped below the horizon", "painting the sky in shades of orange and pink", "A cool breeze rustled the trees", "and the city lights began to twinkle one by one", "People hurried home", "some laughing", "some lost in thought", "as another peaceful evening settled in"];
+
+const randomparagraph = paragraph[Math.floor(Math.random() * paragraph.length)];
+typingtext.innerHTML = '';  
+for(const char of randomparagraph[randompindex]){
+    console.log(char);
+    typingtext.innerHTML+=`<span>${char}</span>`; 
+}
+}
+loadparagraph();
